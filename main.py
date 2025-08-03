e1ca4fb6d0d93e7bf86e96f3e73a89c029f03d89
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2025-08-03 16:32:50 -0400

    POC
    Signed-off-by: Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>

--- main.py
+++ main.py
@@ -13,6 +13,3 @@ async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
